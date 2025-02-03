3e2b1947cc3b4228fea2da13984012585336ad00
ravitez <abhiramanrs200@gmail.com>
2025-02-03 11:04:09 +0530

    added upload button

--- main.py
+++ main.py
@@ -4,6 +4,11 @@ from PIL import Image
 
 with st.expander("start camera"):
     camera_image = st.camera_input("camera")
+    uploaded_image = st.file_uploader("Upload Image")
+if uploaded_image:
+    image = Image.open(uploaded_image)
+    grey_uploaded = image.convert("L")
+    st.image(grey_uploaded)
 if camera_image:
     img = Image.open(camera_image)
     gray_img = img.convert("L")
